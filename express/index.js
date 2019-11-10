7cd95d62735276b092bb8053f824ed5a6d0c56c0
Kasidis Chaowvasin <seai_kasidis@hotmail.com>
2019-11-10 14:10:02 +0700

    init websocket

--- express/index.js
+++ express/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const db = require("./models")
+const expressWs = require("express-ws")(app)
 
 app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
@@ -14,9 +15,18 @@ require("./passport.js")
 // Routing Logic
 const requests = require("./routes/requests")
 const auth = require("./routes/auth")
+const websocket = require("./routes/websocket")
 
 app.use("/request", requests)
 app.use("/auth", auth)
+
+// WebSocket
+app.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+        console.log(msg);
+    })
+    console.log('socket', req.testing)
+})
 
 app.listen(3000, () => {
     db.sequelize.sync()
